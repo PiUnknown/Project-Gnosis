4b8d9a058bd0b22d474563148ff5da60d7de7600
PiUnknown <okjha09@gmail.com>
2026-08-19 21:41:59 +0530

    feat: migrate analytics to PostHog SDK tracking

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,8 +3,20 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+import { PostHogProvider } from '@posthog/react'
+
+const options = {
+  api_host: import.meta.env.VITE_POSTHOG_HOST || "https://eu.i.posthog.com",
+  disable_session_recording: true,
+}
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <PostHogProvider 
+      apiKey={import.meta.env.VITE_POSTHOG_PROJECT_TOKEN || "phc_vEgQtXumCU6NkhgdDiGm5J8MndG4wEXTMuUjXRNYorDP"}
+      options={options}
+    >
+      <App />
+    </PostHogProvider>
   </React.StrictMode>,
 )
